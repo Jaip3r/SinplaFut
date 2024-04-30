ebe1948d34da54dab09d7bcdf9b0e840ad3bb348
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-04-30 14:46:55 -0500

    Inicio del proyecto

---
+++ club-deportivo-service/src/test/java/com/club/service/ClubDeportivoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubDeportivoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
