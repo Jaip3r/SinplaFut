5c8040b04fe1304dd4b4a1eb1c4af221524ab9ae
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-05-30 14:58:46 -0500

    Configuración de balanceador de carga con feign

---
+++ team-service/src/main/java/com/team/service/config/LoadBalancerConfiguration.java
@@ -0,0 +1,21 @@
+package com.team.service.config;
+
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoadBalancerConfiguration {
+
+    @Bean
+    public ServiceInstanceListSupplier serviceInstanceListSupplier(ConfigurableApplicationContext context){
+        log.info("Configurando balanceador de carga");
+        return ServiceInstanceListSupplier
+                .builder()
+                .withBlockingDiscoveryClient() // La obtención de la lista de instancias sera asincrona.
+                .withSameInstancePreference() // prefiere la misma instancia del servicio en llamadas sucesivas.
+                .build(context);
+    }
+    
+}
