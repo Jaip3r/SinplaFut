033a076df88942768d37e71e2eeb9230464c8bed
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-06-08 17:32:01 -0500

    Modificando capa de servicios

--- jugador-service/src/main/java/com/jugador/service/services/JugadorService.java
+++ jugador-service/src/main/java/com/jugador/service/services/JugadorService.java
@@ -3,6 +3,7 @@ package com.jugador.service.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.jugador.service.models.EstadoJugador;
 import com.jugador.service.models.Jugador;
 
 public interface JugadorService {
@@ -10,6 +11,10 @@ public interface JugadorService {
     List<Jugador> findAll();
 
     List<Jugador> findByEquipo(Long equipo_id);
+
+    List<Jugador> findByEstado(EstadoJugador estado);
+
+    Optional<Jugador> findByDocumento(String documento);
 
     Optional<Jugador> findById(Long id);
 
--- jugador-service/src/main/java/com/jugador/service/services/impl/JugadorServiceImpl.java
+++ jugador-service/src/main/java/com/jugador/service/services/impl/JugadorServiceImpl.java
@@ -3,12 +3,16 @@ package com.jugador.service.services.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.jugador.service.models.EstadoJugador;
 import com.jugador.service.models.Jugador;
 import com.jugador.service.persistence.IJugadorDAO;
 import com.jugador.service.services.JugadorService;
 
 import lombok.RequiredArgsConstructor;
 
+@Service
 @RequiredArgsConstructor
 public class JugadorServiceImpl implements JugadorService {
 
@@ -20,6 +24,11 @@ public class JugadorServiceImpl implements JugadorService {
     }
 
     @Override
+    public List<Jugador> findByEstado(EstadoJugador estado) {
+        return this.iJugadorDAO.findByEstado(estado);
+    }
+
+    @Override
     public List<Jugador> findByEquipo(Long equipo_id) {
         return this.iJugadorDAO.findByEquipo(equipo_id);
     }
@@ -27,6 +36,11 @@ public class JugadorServiceImpl implements JugadorService {
     @Override
     public Optional<Jugador> findById(Long id) {
         return this.iJugadorDAO.findById(id);
+    }
+
+    @Override
+    public Optional<Jugador> findByDocumento(String documento) {
+        return this.iJugadorDAO.findByDocumento(documento);
     }
 
     @Override
