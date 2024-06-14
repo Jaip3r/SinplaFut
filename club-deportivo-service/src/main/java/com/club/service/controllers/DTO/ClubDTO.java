8c8b553d041f42f743082a8c129b137425d29d9d
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-06-14 15:23:40 -0500

    Refactorizando código de Dtos

--- club-deportivo-service/src/main/java/com/club/service/controllers/DTO/ClubDTO.java
+++ club-deportivo-service/src/main/java/com/club/service/controllers/DTO/ClubDTO.java
@@ -8,30 +8,30 @@ import jakarta.validation.constraints.Pattern;
 public record ClubDTO (
 
     @NotBlank(message = "El nombre del club es obligatorio")
-    @Length(min = 3, max = 30, message = "El nombre del club debe contener entre 3 y 30 caracteres")
-    @Pattern(regexp = "^[^\\s][a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\\s]*[^\\s]$", message = "El nombre del club no debe contener caracteres especiales")
+    @Length(min = 3, max = 30, message = "El nombre del club debe contener entre 3 y 30 carácteres")
+    @Pattern(regexp = "^[^\\s][a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\\s]*[^\\s]$", message = "El nombre del club no debe contener carácteres especiales")
     String nombre,
 
     @NotBlank(message = "La dirección del club es obligatoria")
-    @Length(min = 3, max = 25, message = "La dirección del club debe contener entre 3 y 25 caracteres")
+    @Length(min = 3, max = 25, message = "La dirección del club debe contener entre 3 y 25 carácteres")
     String direccion,
 
-    @NotBlank(message = "El telefono del club es obligatorio")
-    @Length(min = 7, message = "El telefono del club debe contener 7 numeros")
-    @Pattern(regexp = "^[0-9]+$", message = "El telefono debe contener solo numeros")
+    @NotBlank(message = "El teléfono del club es obligatorio")
+    @Length(min = 7, message = "El teléfono del club debe contener 7 números")
+    @Pattern(regexp = "^[0-9]+$", message = "El telefono debe contener solo números")
     String telefono,
 
     @NotBlank(message = "La ciudad de residencia del club es obligatoria")
-    @Length(min = 3, max = 25, message = "El nombre de la ciudad debe contener entre 3 y 25 caracteres")
+    @Length(min = 3, max = 25, message = "El nombre de la ciudad debe contener entre 3 y 25 carácteres")
     String ciudad,
 
-    @NotBlank(message = "El pais de residencia del club es obligatorio")
-    @Length(min = 3, max = 25, message = "El nombre del pais debe contener entre 3 y 25 caracteres")
+    @NotBlank(message = "El país de residencia del club es obligatorio")
+    @Length(min = 3, max = 25, message = "El nombre del país debe contener entre 3 y 25 carácteres")
     String pais,
 
     @NotBlank(message = "El estadio del club es obligatorio")
-    @Length(min = 3, max = 25, message = "El nombre del estadio debe contener entre 3 y 25 caracteres")
-    @Pattern(regexp = "^(?! )[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$", message = "El nombre del estadio no debe contener caracteres especiales")
+    @Length(min = 3, max = 25, message = "El nombre del estadio debe contener entre 3 y 25 carácteres")
+    @Pattern(regexp = "^(?! )[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]+$", message = "El nombre del estadio no debe contener carácteres especiales")
     String estadio
 
 ) {
