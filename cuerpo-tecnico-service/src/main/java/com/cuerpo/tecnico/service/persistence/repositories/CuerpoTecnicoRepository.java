15a479b35ce9734f9047af2d31f88ad4fdf05227
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-06-24 16:13:29 -0500

    Mejorando comentarios en consultas personalizadas

--- cuerpo-tecnico-service/src/main/java/com/cuerpo/tecnico/service/persistence/repositories/CuerpoTecnicoRepository.java
+++ cuerpo-tecnico-service/src/main/java/com/cuerpo/tecnico/service/persistence/repositories/CuerpoTecnicoRepository.java
@@ -12,14 +12,16 @@ public interface CuerpoTecnicoRepository extends JpaRepository<CuerpoTecnico, Lo
 
     // Query methods to find an especific technical staff
 
-    // Buscar un jugador por correo electrónico o número de documento
+    // Buscar un miembro del staff técnico por correo electrónico o número de documento
     Optional<CuerpoTecnico> findFirstByEmailOrDocumento(String email, String documento);
 
-    // Buscar todos los jugadores que coinciden con correo electrónico o número de documento
-    List<CuerpoTecnico>  findAllByEmailOrDocumento(String email, String documento);
+    // Buscar todos los miembros del staff técnico que coinciden con correo electrónico o número de documento
+    List<CuerpoTecnico> findAllByEmailOrDocumento(String email, String documento);
 
+    // Busca todos los miembros del staff técnico que por tipo y equipo
     List<CuerpoTecnico> findAllByTipoAndEquipoId(CuerpoTecnicoType tipo, Long equipoId);
 
+    // Busca todos los miembros del staff técnico por equipo
     List<CuerpoTecnico> findAllByEquipoId(Long equipo_id);
     
 }
